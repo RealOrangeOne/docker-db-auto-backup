17d0ec2a7619607a10e5422a6a1f9e2e497ac226
Jake Howard <git@theorangeone.net>
2022-06-19 10:51:18 +0100

    Correctly wait for postgres to start
    Now our container stops faster, this actually gets called. Turns out `isready` means exactly that - it doesn't wait until it's ready, so we need to do the looping ourselves

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,7 +10,10 @@ docker-compose stop backup
 
 echo "> Await databases..."
 # Only check postgres, that should be fine
-docker-compose exec -T psql pg_isready -U postgres
+until docker-compose exec -T psql pg_isready -U postgres
+do
+  sleep 1
+done
 
 echo "> Run backups..."
 docker-compose run backup ./db-auto-backup.py
