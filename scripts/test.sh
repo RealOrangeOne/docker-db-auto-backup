39e8b67b51a4bf2c737614f50402b4e3161387a6
Jake Howard <git@theorangeone.net>
2023-08-01 09:18:17 +0100

    Ensure mysql is also running
    This thing takes a long time to start!

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,9 +8,13 @@ docker-compose up -d
 echo "> Stop backup container..."
 docker-compose stop backup
 
-echo "> Await databases..."
-# Only check postgres, that should be fine
+echo "> Await postgres..."
 until docker-compose exec -T psql pg_isready -U postgres
+do
+  sleep 1
+done
+echo "> Await mysql..."
+until docker-compose exec -T mysql bash -c 'mysqladmin ping --protocol tcp -p$MYSQL_ROOT_PASSWORD'
 do
   sleep 1
 done
