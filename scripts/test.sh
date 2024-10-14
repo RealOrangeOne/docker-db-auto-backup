5fca86bfd0f855e713cbb91e60ff4ba9539ecf05
Jake Howard <git@theorangeone.net>
2024-10-14 19:45:56 +0100

    Use `docker compose`

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,16 +5,16 @@ set -e
 export PATH=env/bin:${PATH}
 
 echo "> Start all containers..."
-docker-compose up -d
+docker compose up -d
 
 echo "> Await postgres..."
-until docker-compose exec -T psql pg_isready -U postgres
+until docker compose exec -T psql pg_isready -U postgres
 do
   sleep 1
 done
 
 echo "> Await mysql..."
-until docker-compose exec -T mysql bash -c 'mysqladmin ping --protocol tcp -p$MYSQL_ROOT_PASSWORD'
+until docker compose exec -T mysql bash -c 'mysqladmin ping --protocol tcp -p$MYSQL_ROOT_PASSWORD'
 do
   sleep 3
 done
@@ -22,4 +22,4 @@ done
 pytest -v
 
 echo "> Clean up..."
-docker-compose down -t 3
+docker compose down -t 3
