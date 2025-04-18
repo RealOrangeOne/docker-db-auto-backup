207898fe08ffe6a5b6e6ea7f963f3d5d1f42dd5b
Spacetech <824323+Spacetech@users.noreply.github.com>
2025-04-18 03:38:49 -0700

    Add support for pgvector container (#80)
    Support backups for https://github.com/pgvector/pgvector
    The container base image is `postgres`

--- db-auto-backup.py
+++ db-auto-backup.py
@@ -135,6 +135,7 @@ BACKUP_PROVIDERS: list[BackupProvider] = [
             "tensorchord/pgvecto-rs",
             "nextcloud/aio-postgresql",
             "timescale/timescaledb",
+            "pgvector/pgvector",
         ],
         backup_method=backup_psql,
         file_extension="sql",
